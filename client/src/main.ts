72df24d6be632bf530064fdcf509af61f65cda01
Hilan Meyran <hilanmeyran@protonmail.com>
2024-09-15 20:13:02 +0200

    working status

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,6 +2,7 @@ import { invoke } from '@tauri-apps/api/core';
 import { listen } from '@tauri-apps/api/event';
 import { debug, error, info, warn } from '@tauri-apps/plugin-log';
 import { forwardConsole, forwardUnhandledRejection } from './log';
+import { fetch } from '@tauri-apps/plugin-http';
 
 
 forwardConsole('log', debug);
@@ -13,6 +14,34 @@ forwardConsole('error', error);
 forwardUnhandledRejection(error);
 
 window.addEventListener("DOMContentLoaded", async () => {
+
+  const statusDot = document.getElementById('statusDot') as HTMLSpanElement
+  setInterval(function() {
+    fetch('http://localhost:7331/healthcheck')
+      .then(response => {
+        if (response.ok) {
+          // Server is up
+          statusDot.innerHTML = `
+          <span class="relative inline-flex h-3 w-3 rounded-full bg-green-500"></span>
+          <span class="absolute inline-flex h-full w-full animate-ping rounded-full bg-green-400 opacity-75"></span>
+        `;
+        } else {
+          // Server is down
+          statusDot.innerHTML = `
+          <span class="relative inline-flex h-3 w-3 rounded-full bg-red-500"></span>
+          <span class="absolute inline-flex h-full w-full animate-ping rounded-full bg-red-400 opacity-75"></span>
+        `;
+        }
+      })
+      .catch(error => {
+        // Error occurred, assume server is down
+        console.error(error);
+        statusDot.innerHTML = `
+          <span class="relative inline-flex h-3 w-3 rounded-full bg-red-500"></span>
+          <span class="absolute inline-flex h-full w-full animate-ping rounded-full bg-red-400 opacity-75"></span>
+      `;
+      });
+  }, 3000);
 
   const joinButton = document.getElementById('joinServerButton') as HTMLButtonElement;
   joinButton.addEventListener('click', async () => {
