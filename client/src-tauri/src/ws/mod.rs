79330b61206c7ceb0f16690eb4d94b82614c8db9
Hilan Meyran <hilanmeyran@protonmail.com>
2024-09-16 12:45:44 +0200

    Serializable WebSocketError

--- client/src-tauri/src/ws/mod.rs
+++ client/src-tauri/src/ws/mod.rs
@@ -2,6 +2,7 @@ use std::fmt;
 use std::sync::Arc;
 
 use futures_util::stream::{SplitSink, SplitStream};
+use serde::Serialize;
 use tokio::{net::TcpStream, sync::Mutex};
 use tokio_tungstenite::tungstenite::Error as TungsteniteError;
 use tokio_tungstenite::{tungstenite::Message, MaybeTlsStream, WebSocketStream};
@@ -25,6 +26,27 @@ pub enum WebSocketError {
     ReceiveError(TungsteniteError),
     ParseError(&'static str),
     UnknownError,
+}
+
+impl Serialize for WebSocketError {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        match self {
+            WebSocketError::ConnectionError(e) => {
+                serializer.serialize_str(&format!("ConnectionError: {}", e))
+            }
+            WebSocketError::SendError(e) => serializer.serialize_str(&format!("SendError: {}", e)),
+            WebSocketError::ReceiveError(e) => {
+                serializer.serialize_str(&format!("ReceiveError: {}", e))
+            }
+            WebSocketError::ParseError(e) => {
+                serializer.serialize_str(&format!("ParseError: {}", e))
+            }
+            WebSocketError::UnknownError => serializer.serialize_str("UnknownError"),
+        }
+    }
 }
 
 impl fmt::Display for WebSocketError {
