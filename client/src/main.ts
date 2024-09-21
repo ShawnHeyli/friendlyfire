089994b20870be58326bef9c0b456b37aa9b4145
Hilan Meyran <hilanmeyran@protonmail.com>
2024-09-21 18:34:36 +0200

    update avatar placeholder with initials

--- client/src/main.ts
+++ client/src/main.ts
@@ -47,6 +47,24 @@ async function initStatusDot(endpoint: string, interval: number) {
 
 }
 
+function initUpdateAvatarPlaceHolder() {
+  const usernameInput = document.getElementById('usernameInput') as HTMLInputElement;
+  const avatarPlaceholder = document.getElementById('avatarLetter') as HTMLSpanElement;
+
+  usernameInput.addEventListener("input", () => {
+    const username = usernameInput.value.trim();
+    if (username.length > 0) {
+      const words = username.split(" ");
+      const initials = words.slice(0, 2).map(word => word[0]).join(''); // Takes the first letter of two words
+      avatarPlaceholder.textContent = initials.toUpperCase();
+    } else {
+      avatarPlaceholder.textContent = '';
+    }
+  })
+
+}
+
 window.addEventListener("DOMContentLoaded", () => {
-  initStatusDot("http://localhost:7331/healthcheck", 3000)
+  initStatusDot("http://localhost:7331/healthcheck", 3000);
+  initUpdateAvatarPlaceHolder();
 });
