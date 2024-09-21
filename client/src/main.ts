e2ffd7eae86fcd665fff5b6dea0ce3ef0b8a0d9f
Hilan Meyran <hilanmeyran@protonmail.com>
2024-09-21 17:53:09 +0200

    working server status dot

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,22 +1,52 @@
-import { invoke } from "@tauri-apps/api/core";
+import { fetch } from "@tauri-apps/plugin-http";
 
-let greetInputEl: HTMLInputElement | null;
-let greetMsgEl: HTMLElement | null;
+async function initStatusDot(endpoint: string, interval: number) {
+  const forwardDot = document.getElementById('forwardDot') as HTMLSpanElement
+  const backDot = document.getElementById('backDot') as HTMLSpanElement
+  setInterval(function() {
+    backDot.classList.add('animate-ping');
+    fetch(endpoint)
+      .then(response => {
+        if (response.ok) {
+          // Server is up, pulse the status dot
+          forwardDot.classList.remove('bg-gray-500');
+          forwardDot.classList.remove('bg-red-500');
+          forwardDot.classList.add('bg-green-500');
+          backDot.classList.remove('bg-gray-400');
+          backDot.classList.remove('bg-red-400');
+          backDot.classList.add('bg-green-400');
+          setTimeout(() => {
+            backDot.classList.remove('animate-ping');
+          }, 600); // Remove the pulse after interval
+        } else {
+          // Server is down, set the status dot to red
+          forwardDot.classList.remove('bg-gray-500');
+          forwardDot.classList.remove('bg-green-500');
+          forwardDot.classList.add('bg-red-500');
+          backDot.classList.remove('bg-gray-400');
+          backDot.classList.remove('bg-green-400');
+          backDot.classList.add('bg-red-400');
+          setTimeout(() => {
+            backDot.classList.remove('animate-ping');
+          }, 600); // Remove the pulse after interval
+        }
+      })
+      .catch(_error => {
+        // Error occurred, assume server is down
+        forwardDot.classList.remove('bg-gray-500');
+        forwardDot.classList.remove('bg-green-500');
+        forwardDot.classList.add('bg-red-500');
+        backDot.classList.remove('bg-gray-400');
+        backDot.classList.remove('bg-green-400');
+        backDot.classList.add('bg-red-400');
+        setTimeout(() => {
+          backDot.classList.remove('animate-ping');
+        }, 600); // Remove the pulse after interval
+      });
+  }, interval);
 
-async function greet() {
-  if (greetMsgEl && greetInputEl) {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    greetMsgEl.textContent = await invoke("greet", {
-      name: greetInputEl.value,
-    });
-  }
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form")?.addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
-  });
+  initStatusDot("http://localhost:7331/healthcheck", 3000)
 });
