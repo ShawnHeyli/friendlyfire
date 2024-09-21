d26f443117b9ebb30b30597850c9eb5477ba30d3
Hilan Meyran <hilanmeyran@protonmail.com>
2024-09-21 13:56:24 +0200

    initial commit

--- client/src-tauri/src/lib.rs
+++ client/src-tauri/src/lib.rs
@@ -1,127 +1,14 @@
-use std::path::PathBuf;
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
 
-use log::{debug, info};
-use play::image::{self, ImagePayload};
-use play::video::{self, VideoPayload};
-use play::{upload_file, Sendable};
-use tauri::{AppHandle, Manager, Url, WebviewWindow};
-use tauri_plugin_log::fern::colors::ColoredLevelConfig;
-use tokio::fs::File;
-use tokio_tungstenite::tungstenite::Message;
-use ws::close::close_ws_connection;
-use ws::init::init_ws_connection;
-use ws::messages::send_ws_message;
-use ws::WebSocketError;
-
-pub mod play;
-pub mod store;
-pub mod ws;
-
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(
-            tauri_plugin_log::Builder::new()
-                .with_colors(ColoredLevelConfig::default())
-                .level(log::LevelFilter::Debug)
-                .build(),
-        )
-        .plugin(tauri_plugin_websocket::init())
-        .plugin(tauri_plugin_dialog::init())
-        .plugin(tauri_plugin_http::init())
-        .plugin(tauri_plugin_fs::init())
-        .plugin(tauri_plugin_store::Builder::new().build())
-        .invoke_handler(tauri::generate_handler![
-            join_server,
-            leave_server,
-            send_ws_string,
-            play_image,
-            play_video,
-        ])
-        .setup(|app| {
-            #[cfg(desktop)]
-            app.handle()
-                .plugin(tauri_plugin_positioner::init())
-                .unwrap();
-
-            rustls::crypto::ring::default_provider()
-                .install_default()
-                .expect("Failed to install rustls crypto provider");
-
-            Ok(())
-        })
+        .plugin(tauri_plugin_shell::init())
+        .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
-}
-
-#[tauri::command]
-async fn join_server(handle: AppHandle) -> Result<(), WebSocketError> {
-    init_ws_connection(handle).await
-    // From here WS_CONNECTION is set
-    // After this client receives joined message and updates the client count
-}
-
-#[tauri::command]
-async fn leave_server() -> Result<(), WebSocketError> {
-    close_ws_connection().await
-}
-
-#[tauri::command]
-async fn send_ws_string(message: String) {
-    send_ws_message(Message::Text(message)).await.unwrap();
-}
-
-#[tauri::command]
-async fn play_image(handle: AppHandle, path: PathBuf, text: String) {
-    info!("{:?}", path);
-    let file = File::open(&path).await.unwrap();
-    let (width, height) = image::dimensions(&path).unwrap();
-    let window = handle.get_webview_window("player").unwrap();
-    let (width, height) = calculate_image_size(&window, width, height);
-    let remote_path = upload_file(file).await;
-    debug!("Received remote_path '{}' from the server", remote_path);
-    let remote_path =
-        Url::parse(format!("http://localhost:7331/uploads/{}", remote_path).as_str()).unwrap();
-    let payload = ImagePayload::new(remote_path, text.clone(), width, height);
-    payload.send().await;
-}
-
-#[tauri::command]
-async fn play_video(handle: AppHandle, path: PathBuf, text: String) {
-    let file = File::open(&path).await.unwrap();
-    let (width, height) = video::dimensions(&path).unwrap();
-    let window = handle.get_webview_window("player").unwrap();
-    let (width, height) = calculate_image_size(&window, width, height);
-    let remote_path = upload_file(file).await;
-    debug!("Received remote_path '{}' from the server", remote_path);
-    let remote_path =
-        Url::parse(format!("http://localhost:7331/uploads/{}", remote_path).as_str()).unwrap();
-    let payload = VideoPayload::new(remote_path, text.clone(), width, height);
-    payload.send().await;
-}
-
-fn calculate_image_size(
-    window: &WebviewWindow,
-    original_width: f64,
-    original_height: f64,
-) -> (f64, f64) {
-    let binding = window.current_monitor().unwrap().unwrap();
-    let screen_size = binding.size();
-    let screen_width = screen_size.width as f64;
-    let screen_height = screen_size.height as f64;
-
-    let aspect_ratio = original_width / original_height;
-    let max_width = screen_width * 0.4;
-    let max_height = screen_height * 0.4;
-
-    let (width, height) = if original_width > max_width || original_height > max_height {
-        if original_width > original_height {
-            (max_width, max_width / aspect_ratio)
-        } else {
-            (max_height * aspect_ratio, max_height)
-        }
-    } else {
-        (original_width, original_height)
-    };
-
-    (width, height)
 }
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,78 +1,22 @@
-import { invoke } from '@tauri-apps/api/core';
-import { listen } from '@tauri-apps/api/event';
-import { error } from '@tauri-apps/plugin-log';
-import { forwardUnhandledRejection } from './log';
-import { open } from '@tauri-apps/plugin-dialog';
-import * as fileType from 'file-type';
-import { readFile } from '@tauri-apps/plugin-fs';
-import { initServerStatus } from './serverStatus';
+import { invoke } from "@tauri-apps/api/core";
 
+let greetInputEl: HTMLInputElement | null;
+let greetMsgEl: HTMLElement | null;
 
-// forwardConsole('log', debug);
-// forwardConsole('debug', debug);
-// forwardConsole('info', info);
-// forwardConsole('warn', warn);
-// forwardConsole('error', error);
+async function greet() {
+  if (greetMsgEl && greetInputEl) {
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    greetMsgEl.textContent = await invoke("greet", {
+      name: greetInputEl.value,
+    });
+  }
+}
 
-forwardUnhandledRejection(error);
-
-window.addEventListener("DOMContentLoaded", async () => {
-
-  initServerStatus(3000);
-
-  let connected = false;
-  const serverButton = document.getElementById('serverButton') as HTMLButtonElement;
-  serverButton.addEventListener("click", async () => {
-    if (!connected) {
-      invoke('join_server').then(() => {
-        const connectionStatus = document.getElementById("connectionStatus") as HTMLSpanElement;
-        connectionStatus.innerHTML = "Connected";
-        connected = true;
-        serverButton.innerHTML = "Leave server"
-      })
-    } else if (connected) {
-      invoke('leave_server').then(() => {
-        const connectionStatus = document.getElementById("connectionStatus") as HTMLSpanElement;
-        connectionStatus.innerHTML = "Disconnected";
-        const clientCounter = document.getElementById('clientCount') as HTMLSpanElement;
-        clientCounter.style.setProperty('--value', "0");
-        connected = false;
-        serverButton.innerHTML = "Join server"
-      })
-    }
-  });
-
-  const playButton = document.getElementById('playButton') as HTMLButtonElement;
-  playButton.addEventListener("click", async () => {
-    if (connected) {
-      //@ts-ignore
-      let file: string | null = await open({
-        multiple: false,
-        filters: [{ name: 'Media', extensions: ['jpg', 'jpeg', 'png', 'gif', 'mp4', 'webm', 'ogg'] }]
-      });
-      if (file) {
-        const content = await readFile(file);
-        const type = await fileType.fileTypeFromBuffer(content);
-        if (type && type.mime.startsWith('image/')) {
-          const textInput = document.getElementById('textInput') as HTMLInputElement;
-          const text = textInput.value;
-          invoke('play_image', { path: file, text });
-        } else if (type && type.mime.startsWith('video/')) {
-          const textInput = document.getElementById('textInput') as HTMLInputElement;
-          const text = textInput.value;
-          invoke('play_video', { path: file, text });
-        } else {
-          console.error('Unsupported file type:', type ? type.mime : 'unknown');
-        }
-      }
-    }
-  });
-
-
-  listen<JoinMessage>('updateClientCount', (data) => {
-    const payload: JoinMessage = data.payload;
-    const clientCounter = document.getElementById('clientCount') as HTMLSpanElement;
-    clientCounter.style.setProperty('--value', payload.clientCount.toString());
+window.addEventListener("DOMContentLoaded", () => {
+  greetInputEl = document.querySelector("#greet-input");
+  greetMsgEl = document.querySelector("#greet-msg");
+  document.querySelector("#greet-form")?.addEventListener("submit", (e) => {
+    e.preventDefault();
+    greet();
   });
 });
-
