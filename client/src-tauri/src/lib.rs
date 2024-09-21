7c040b47da7cbe06765d5da757772d1f7ddfdccb
Hilan Meyran <hilanmeyran@protonmail.com>
2024-09-21 17:52:26 +0200

    remove shell plugin

--- client/src-tauri/src/lib.rs
+++ client/src-tauri/src/lib.rs
@@ -2,7 +2,6 @@
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_http::init())
-        .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
