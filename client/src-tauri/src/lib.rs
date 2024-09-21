42612c43fbd4fc3e38215cc6965f6b2e68a16057
Hilan Meyran <hilanmeyran@protonmail.com>
2024-09-21 21:33:42 +0200

    remember window state

--- client/src-tauri/src/lib.rs
+++ client/src-tauri/src/lib.rs
@@ -1,6 +1,7 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_window_state::Builder::new().build())
         .plugin(tauri_plugin_http::init())
         .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
