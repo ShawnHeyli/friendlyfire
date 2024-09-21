5f9182def890f6dd1e2599aa9104c17000a130f7
Hilan Meyran <hilanmeyran@protonmail.com>
2024-09-21 14:20:42 +0200

    wayland fix

--- client/src-tauri/src/main.rs
+++ client/src-tauri/src/main.rs
@@ -1,6 +1,11 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::env;
+
 fn main() {
+    env::set_var("GDK_BACKEND", "x11");
+    env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+
     friendlyfire_lib::run()
 }
