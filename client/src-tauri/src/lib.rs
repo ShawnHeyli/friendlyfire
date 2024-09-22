95af45355823e5757a82959241055082a6af4f98
Hilan Meyran <hilanmeyran@protonmail.com>
2024-09-22 11:38:48 +0200

    re-add window-state save init

--- client/src-tauri/src/lib.rs
+++ client/src-tauri/src/lib.rs
@@ -36,6 +36,7 @@ pub type WebSocketSplitSink = SplitSink<WebSocketStream<MaybeTlsStream<TcpStream
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_window_state::Builder::new().build())
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(
             tauri_plugin_log::Builder::new()
