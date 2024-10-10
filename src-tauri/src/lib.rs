ee0556140f102b36b2a95c7a3029e2514312fbd9
Hilan Meyran <hilanmeyran@protonmail.com>
2024-10-10 14:13:41 -0700

    add clipboard plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -25,6 +25,7 @@ struct ConnectionState {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_clipboard::init())
         .plugin(tauri_plugin_window_state::Builder::new().build())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
